11734594014d5efef399dfe089b681dfe47dc9d0
Rayfun <42323982+milai@users.noreply.github.com>
2019-04-23 21:21:07 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY")); //这里改成您的api key
+define('APIKEY', getenv("AIzaSyCsJp53kmo6rGha38d18yMJFafuGSiR7jI")); //这里改成您的api key
 define('GJ_CODE', 'TW');//国家代码
 define('SITE_NAME', 'Youtube代理');//网站名字
 define('TITLENAME', 'Youtube代理');//网站名字
-define('EN2DEKEY', 'gfw-breaker');//一个随机字符串，加解密KEY
-define('EMAIL', 'lixiaoming0909@gmail.com');//邮箱
-define('PLAYLIST_ID', getenv("PLAYLIST_ID"));//推荐内容播放列表
+define('EN2DEKEY', 'breaker');//一个随机字符串，加解密KEY
+define('EMAIL', 'zz688700@gmail.com');//邮箱
 ?>
